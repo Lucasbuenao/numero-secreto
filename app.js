4316dc9d9ba81cea5eee49935f74b60a76dd05e3
Lucas Bueno <Lucasbuenao>
2024-03-14 02:59:57 -0300

    alterando limite

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = numAleatorio();
 let tentativas = 1;
 let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
